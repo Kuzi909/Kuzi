7531402fd99be73c06abcfb40dfa405ee4196c2c
kilin <wl00161839@gmail.com>
2023-02-22 16:24:48 +0800

    遊戲

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,32 @@
 var dic;
 var king;
+let kilin;
 //var move_div = $('#move_div');
 //var main_table = $('#main_table');
-
+king="H_16";
 $(document).ready(function(){
     var mainTable = $('#main_table');
-        for(var i = 1; i <= 13; i++){
+        for(var i = 1; i <= 15; i++){
             mainTable.append("<tr></tr>");
             for(var j = 1; j <= 31; j++){
                 mainTable.append("<td id='" + abc(i) + "_" + j + "'></td>");
             }
         }
-
-    $("td").on("click",(object)=>{
-        //king = object.attr("id");
-        king = object.target.id;
-        console.log(object.target.id);
-        console.log(object);
         move();
+    $("td").on("click",(object)=>{
+        if($($(object)[0].currentTarget).attr("style")=="background-color:yellow;"){
+            king=$($(object)[0].currentTarget).attr("id");
+            move();
+        }else{
+            console.log("no");
+        }
+        
     })
 })
 
 function addTable(){
     var mainTable = $('#main_table');
-    for(var i = 1; i <= 13; i++){
+    for(var i = 1; i <= 14; i++){
         mainTable.append("<tr></tr>");
         for(var j = 1; j <= 31; j++){
             mainTable.append("<td id='" + abc(i) + "_" + j + "'></td>");
@@ -32,9 +35,20 @@ function addTable(){
 }
 
 function dice(){
-    var dic = Math.floor(Math.random() * 24) + 1;
+    var dic = Math.floor(Math.random() * 6) + 1;
     var user_dice_div = $('#user_dice_div');
     user_dice_div.html("<span>" + dic + "</span>");
+    let upcom=king.split("_")[0].charCodeAt(0)-65;
+    let leftcom=king.split("_")[1];
+    for(let i=((upcom-dic)<0?0:(upcom-dic));i<=((upcom+dic)>14?14:(upcom+dic));i++){
+        let far=(upcom-i)<0?-(upcom-i):(upcom-i);
+        for(j=((leftcom-(dic-far))<0?0:(leftcom-(dic-far)));j<=((parseInt(leftcom)+(dic-far))>31?31:(parseInt(leftcom)+(dic-far)));j++){
+            $("#"+(String.fromCharCode(i+65))+"_"+j)
+                .attr("style","background-color:yellow;");
+        }
+    }
+    
+    $("#"+king).attr("style","background-color:black;");
 }
 
 function start(){
@@ -55,11 +69,12 @@ function start(){
 }
 
 function move(){
-    for(let i=king.split("_")[0].charCodeAt(0)-65-dic; dic; i++){
-        for(let j=king.split("_"[1]-dic); dic; j++){
-           console.log(i+":"+j);
-        }
-    }
+    $("td").each((a,b)=>{
+        $(b).attr("style","");
+    })
+
+    $("#"+king).attr("style","background-color:black;");
+
 }
 
 function move2(){
